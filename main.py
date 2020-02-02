f5251e3e259bb23cd8c03dfd698d1eb152a3facb
A Meade <snickeredyt@gmail.com>
2020-02-02 10:50:24 +0000

    main calls takepicture

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 import time
+from src.takepicture import takePicture
 
-def take_picture():
-    print("pic taken")
+#def take_picture():
+#    print("pic taken")
 # TODO main executable file, pulls in cadets' libraries from lib fold
 def main():
     while 1==1:
-        take_picture()
+        takePicture(1)
         time.sleep(5)
-    
+
 
 if __name__ == '__main__':
     main()
-
