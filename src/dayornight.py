627f493b6638a9ea6b5c1834813404c5cfa70f6c
George Connellan <georgeconnellan@hotmail.com>
2020-02-02 14:15:03 +0000

    upgrade day detection with reading files

--- src/dayornight.py
+++ src/dayornight.py
@@ -2,7 +2,7 @@
 import numpy as np
 
 def isDay(imageFile):
-    fd = open(imageFile'image.data', 'rb')
+    fd = open(imageFile, 'rb')
     f=np.fromfile(fd, dtype=np.uint8, count=2592*1944)
     lum = 0
     count = 0
@@ -10,8 +10,10 @@ def isDay(imageFile):
     miny = 4000000
     maxy = -1
     
-    while count*3 < len (f):
-        y = f[count*3]
+    print("Size of array (data points): " + str(len(f)))
+    
+    while count*3*16 < len (f):
+        y = f[count*3*16]
         lum = y + lum
         count = count + 1
         
@@ -26,6 +28,7 @@ def isDay(imageFile):
     print (str(miny)+" is minimum y value")
     print (str(maxy)+" is maximum y value")
     print (str(avg)+" is the average y value")
+    print (str(count)+" is the number of pixels analysed")
     
     
     
--- testdaynight.py
+++ testdaynight.py
@@ -1,12 +1,21 @@
 #!/usr/bin/python3
 import time
+from os import listdir
+
 from src.dayornight import isDay
 
 #def take_picture():
 #    print("pic taken")
 # TODO main executable file, pulls in cadets' libraries from lib fold
 def main():
-    dayOrNight("spikes/images/portal1.jpg")
+    t0 = time.time()
+    isDay("spikes/image-1.jpg")
+    t1 = time.time()
+    t = t1 - t0
+    print (str(t))
+    # Now process a folder of image...
+    files = listdir ("spikes/images")
+    
 
 
 if __name__ == '__main__':
