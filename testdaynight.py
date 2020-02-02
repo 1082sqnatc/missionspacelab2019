418319759f5e8981e2ef6ec75af55b2f390f8d86
George Connellan <georgeconnellan@hotmail.com>
2020-02-02 13:07:40 +0000

    added day detection

---
+++ testdaynight.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import time
+from src.dayornight import isDay
+
+#def take_picture():
+#    print("pic taken")
+# TODO main executable file, pulls in cadets' libraries from lib fold
+def main():
+    dayOrNight("spikes/images/portal1.jpg")
+
+
+if __name__ == '__main__':
+    main()
