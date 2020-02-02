1f7fe3e1a5ba6617bf6bc9cb1cfa8c00ed2d7a34
George Connellan <georgeconnellan@hotmail.com>
2020-02-02 09:55:29 +0000

    created control programme

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+def take_picture():
+    return
+# TODO main executable file, pulls in cadets' libraries from lib fold
+def main():
+    take_picture()
 
-# TODO main executable file, pulls in cadets' libraries from lib folder
-
+if __name__ == '__main__':
+    main()
