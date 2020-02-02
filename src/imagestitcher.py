346590cbdc09240da0d8257e88d9acb8042b4e28
A Meade <snickeredyt@gmail.com>
2020-02-02 15:01:26 +0000

    added stitcher

--- src/imagestitcher.py
+++ src/imagestitcher.py
@@ -9,7 +9,7 @@ ap.add_argument("-i", "--images", type=str, required=True,
     help="path to import directory of images to stitch")
 ap.add_argument("-o", "--output" , type=str, required=True,
     help="path to the output image")
-args = vars9ap.parse_args())
+args = vars9ap.parse_args()
 
 print("[INFO] loading images...")
 imagePaths = sorted(list(paths.list_images(args["images"])))
